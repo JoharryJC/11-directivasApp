6282fb561ed654079dfa602f8fdde91991e0b09e
JoharryJC <energycuenta@yahoo.com>
2022-12-20 17:58:57 +0100

    formulario reactivo tradicional

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html'
 })
 export class AgregarComponent {
+
+  miFormulario: FormGroup = this.fb.group ({
+    nombre: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  tieneError (campo: string) : boolean {
+    return this.miFormulario.get(campo)?.invalid || false; 
+  }
 
 }
